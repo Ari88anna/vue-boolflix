9c4c572da9395d19a8e4f1d500ca75cd07d3941a
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-29 16:49:54 +0200

    milestone 1 fatta

--- js/script.js
+++ js/script.js
@@ -3,26 +3,27 @@ var app = new Vue({
     data: {
         movieCollection: [],
         userInput: '',
+        
             
     },
     methods: {
-        searchMovie() {
 
-        }
+        searchMovie()  {
+            axios
+                .get('https://api.themoviedb.org/3/search/movie?', {
+                    params: {
+                        'api_key': '5081a73eac2719896b2d02d515f944ac',
+                        'query': this.userInput,
+                        'page': 1
+                    }
+                })
+                .then((response) => {                
+                    this.movieCollection = response.data.results
+                    console.log(this.movieCollection)
+                });
+        },
+        
     },
-    mounted() {
-        axios
-            .get('https://api.themoviedb.org/3/search/movie?', {
-                params: {
-                    'api_key': '5081a73eac2719896b2d02d515f944ac',
-                    'query': 'smetto quando voglio',
-                    'page': 1
-                }
-            })
-            .then((response) => {                
-                this.movieCollection = response.data.results
-                console.log(this.movieCollection)
-            })
-    }
+    
   })
 
