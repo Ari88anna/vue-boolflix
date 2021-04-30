a155969c95ac52b2cf92ca627a73948614739167
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-30 10:05:22 +0200

    allargata ricerca a serie tv

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 var app = new Vue({
     el: '#root',
     data: {
+        // array vuoto. sarà popolato da ciò che torna dalla chiamata API
         movieCollection: [],
-        userInput: '',
-        
+        // variabile (corrispondente al v-model dell'input da passare al parametro 'query' della API)
+        userInput: '',   
             
     },
     methods: {
 
+        // funzione che ci permette di interrogare le API al click o al keyup
         searchMovie()  {
             axios
-                .get('https://api.themoviedb.org/3/search/movie?', {
+                .get('https://api.themoviedb.org/3/search/multi?', {
                     params: {
                         'api_key': '5081a73eac2719896b2d02d515f944ac',
                         'query': this.userInput,
